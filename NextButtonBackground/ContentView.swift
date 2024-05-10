860d43d9ff90945b801e1a06731b02bce65d57f3
Denys Okhremenko <denys.okhremenko@makeheadway.com>
2024-05-10 15:40:13 +0300

    task

--- NextButtonBackground/ContentView.swift
+++ NextButtonBackground/ContentView.swift
@@ -8,14 +8,44 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var animate: Bool = false
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        Button {
+            withAnimation(.interpolatingSpring(stiffness: 170, damping: 15)) {
+                animate = true
+            } completion: {
+                animate = false
+            }
+            
+        } label : {
+            GeometryReader { proxy in
+                
+                let width = proxy.size.width / 2
+                let sysName = "play.fill"
+                
+                HStack(alignment: .center, spacing: 0) {
+                    Image(systemName: sysName)
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: animate ? width: .zero)
+                        .opacity(animate ? 1 : .zero)
+                    Image(systemName: sysName)
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: width)
+                    Image(systemName: sysName)
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: animate ? 0.5 : width)
+                        .opacity(animate ? .zero : 1)
+                }
+                .frame(maxHeight: .infinity, alignment: .center)
+            }
         }
-        .padding()
+        .frame(maxWidth: 100)
+        .buttonStyle(BackgroundStyle())
+        .foregroundColor(.black)
     }
 }
 
