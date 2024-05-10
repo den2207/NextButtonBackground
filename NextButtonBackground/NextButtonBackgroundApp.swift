6b56477437ea03917932d75e38985f2e34129b05
Denys Okhremenko <denys.okhremenko@makeheadway.com>
2024-05-10 12:08:05 +0300

    Initial Commit

---
+++ NextButtonBackground/NextButtonBackgroundApp.swift
@@ -0,0 +1,17 @@
+//
+//  NextButtonBackgroundApp.swift
+//  NextButtonBackground
+//
+//  Created by Denys Okhremenko on 10.05.2024.
+//
+
+import SwiftUI
+
+@main
+struct NextButtonBackgroundApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
